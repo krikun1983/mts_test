aa1c2d887e4e19be3682396e3f0c537301d4187d
Stanislav <akhmadeevst@gmail.com>
2021-09-27 13:38:31 +0400

    feat: add active link

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import uuid from 'react-uuid';
 import { pathSidebarMenu, sidebarMenu } from '../../constants/pages';
 import { adminShowClose } from '../../store/reducers/adminShowReducer';
 
 const Sidebar = (): JSX.Element => {
   const dispatch = useDispatch();
+  const location = useLocation();
+  const currentPath = location.pathname;
 
   const handleAdminShow = (): void => {
     dispatch(adminShowClose());
@@ -18,7 +20,7 @@ const Sidebar = (): JSX.Element => {
         {pathSidebarMenu.map((path: string, index: number): JSX.Element => {
           return (
             <li className="aside__item" key={uuid()}>
-              <Link className="aside__link" to={path}>
+              <Link className={`aside__link ${currentPath.endsWith(path) ? 'aside__link_active' : ''}`} to={path}>
                 {sidebarMenu[index]}
               </Link>
             </li>
